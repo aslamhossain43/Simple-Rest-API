ff9c9fca169a8ca3d1922de085af281108347aa9
Atif Aslam <aslam.renu.soft43@gmail.com>
2018-11-18 23:36:52 -0800

    Update completed

--- Simple-Rest-API/src/main/java/com/renu/s_r_api/controller/BoookController.java
+++ Simple-Rest-API/src/main/java/com/renu/s_r_api/controller/BoookController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.renu.s_r_api.models.Book;
 import com.renu.s_r_api.repositories.BookRepository;
-
+@CrossOrigin("*")
 @RestController
 public class BoookController {
 private static final Logger LOGGER=LoggerFactory.getLogger(BoookController.class);
